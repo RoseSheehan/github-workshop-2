508f5fea81374607b1f1f7d70c5606a1b23afaf3
RoseSheehan <rosesheehan38@gmail.com>
2021-04-17 19:09:08 -0400

    deleted a function from fun.py

--- fun.py
+++ fun.py
@@ -1,14 +1,9 @@
 # a fun little program
 
-# prints the numbers 1 through 9
-def printNums():
-	for x in range(1,10):
-		print(x)
-
 # prints myString on subsequent lines from 1 to n times
 def duplicateStrings (myString, n):
 	for x in range(1, n):
 		print(myString * x)
 
-printNums()
 duplicateStrings("cat", 5);
+duplicateString("dog", 3);
