15d1e9d64cab19f6e07a0cdc7bbe998f62d42248
RoseSheehan <rosesheehan38@gmail.com>
2021-04-18 11:25:43 -0400

    removed fun.py so I can readd for demo

--- fun.py
+++
@@ -1,9 +0,0 @@
-# a fun little program
-
-# prints myString on subsequent lines from 1 to n times
-def duplicateStrings (myString, n):
-	for x in range(1, n):
-		print(myString * x)
-
-duplicateStrings("cat", 5);
-duplicateString("dog", 3);
